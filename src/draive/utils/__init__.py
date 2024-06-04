3e9e2336eb1d4c33f3e95a302ffdc4d3ec16ba18
Kacper Kaliński <kacper.kalinski@miquido.com>
2024-06-04 13:30:12 +0200

    Add timeout function wrapper

--- src/draive/utils/__init__.py
+++ src/draive/utils/__init__.py
@@ -6,6 +6,7 @@ from draive.utils.mimic import mimic_function
 from draive.utils.missing import MISSING, Missing, is_missing, not_missing
 from draive.utils.split_sequence import split_sequence
 from draive.utils.stream import AsyncStream
+from draive.utils.timeout import timeout
 
 __all__ = [
     "AsyncStream",
@@ -23,4 +24,5 @@ __all__ = [
     "not_missing",
     "setup_logging",
     "split_sequence",
+    "timeout",
 ]
