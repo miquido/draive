e619e98380ea57ce042eada12c240b2556d67c3c
Kacper Kaliński <47140412+KaQuMiQ@users.noreply.github.com>
2024-06-20 08:30:15 +0200

    Add mistral tokenizer

--- src/draive/mistral/__init__.py
+++ src/draive/mistral/__init__.py
@@ -3,10 +3,12 @@ from draive.mistral.config import MistralChatConfig, MistralEmbeddingConfig
 from draive.mistral.embedding import mistral_embed_text
 from draive.mistral.errors import MistralException
 from draive.mistral.lmm import mistral_lmm_invocation
+from draive.mistral.tokenization import mistral_tokenize_text
 
 __all__ = [
     "mistral_embed_text",
     "mistral_lmm_invocation",
+    "mistral_tokenize_text",
     "MistralChatConfig",
     "MistralClient",
     "MistralEmbeddingConfig",
