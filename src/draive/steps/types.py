f5df576ea180c55563d92ef63b1ad8ef62c27e64
Kacper Kaliński <kacper.kalinski@miquido.com>
2024-12-04 19:01:53 +0100

    Allow completed steps and fix image generation

--- src/draive/steps/types.py
+++ src/draive/steps/types.py
@@ -1,10 +1,10 @@
 from collections.abc import Callable, Iterable, Mapping
-from typing import Any, Protocol, Self, runtime_checkable
+from typing import Any, Protocol, Self, overload, runtime_checkable
 
 from haiway import State
 
 from draive.instructions import Instruction
-from draive.lmm import AnyTool, LMMOutputSelection, Toolbox
+from draive.lmm import AnyTool, LMMCompletion, LMMOutputSelection, Toolbox
 from draive.multimodal import Multimodal, MultimodalContent
 
 __all__ = [
@@ -22,6 +22,37 @@ class StepResultProcessing(Protocol):
 
 
 class Step(State):
+    @overload
+    @classmethod
+    def of(
+        cls,
+        input: Multimodal,
+        /,
+        *,
+        completion: Multimodal,
+        volatile: bool = False,
+        extends_result: bool = False,
+        condition: Callable[[], bool] | None = None,
+        **extra: Any,
+    ) -> Self: ...
+
+    @overload
+    @classmethod
+    def of(
+        cls,
+        input: Multimodal,
+        /,
+        *,
+        instruction: Instruction | str | None = None,
+        tools: Toolbox | Iterable[AnyTool] | None = None,
+        output: LMMOutputSelection = "auto",
+        volatile: bool = False,
+        extends_result: bool = False,
+        condition: Callable[[], bool] | None = None,
+        result_processing: StepResultProcessing | None = None,
+        **extra: Any,
+    ) -> Self: ...
+
     @classmethod
     def of(  # noqa: PLR0913
         cls,
@@ -31,19 +62,31 @@ class Step(State):
         instruction: Instruction | str | None = None,
         tools: Toolbox | Iterable[AnyTool] | None = None,
         output: LMMOutputSelection = "auto",
+        completion: Multimodal | None = None,
         volatile: bool = False,
-        inclusive: bool = False,
+        extends_result: bool = False,
         condition: Callable[[], bool] | None = None,
         result_processing: StepResultProcessing | None = None,
         **extra: Any,
     ) -> Self:
+        assert (  # nosec: B101
+            completion is None or instruction is None
+        ), "Can't specify instruction with predefined result"
+        assert (  # nosec: B101
+            completion is None or tools is None
+        ), "Can't specify tools with predefined result"
+        assert (  # nosec: B101
+            completion is None or result_processing is None
+        ), "Can't specify result processing with predefined result"
+
         return cls(
             instruction=Instruction.of(instruction) if instruction else None,
             input=MultimodalContent.of(input),
             toolbox=Toolbox.of(tools),
             output=output,
+            completion=None if completion is None else LMMCompletion.of(completion),
             volatile=volatile,
-            inclusive=inclusive,
+            extends_result=extends_result,
             condition=condition,
             result_processing=result_processing,
             extra=extra,
@@ -53,8 +96,9 @@ class Step(State):
     input: MultimodalContent
     toolbox: Toolbox
     output: LMMOutputSelection
+    completion: LMMCompletion | None
     volatile: bool
-    inclusive: bool
+    extends_result: bool
     condition: Callable[[], bool] | None
     result_processing: StepResultProcessing | None
     extra: Mapping[str, Any]
