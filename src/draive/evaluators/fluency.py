653aba9355eb84559e97a2da0da335271bc3f005
Kacper Kaliński <kacper.kalinski@miquido.com>
2025-10-16 11:07:29 +0200

    Add score name striping for evaluators

--- src/draive/evaluators/fluency.py
+++ src/draive/evaluators/fluency.py
@@ -66,7 +66,7 @@ async def fluency_evaluator(
 
     if result := completion.tag("RESULT"):
         return EvaluationScore.of(
-            cast(EvaluationScoreValue, result.content.to_str().lower()),
+            cast(EvaluationScoreValue, result.content.to_str().strip().lower()),
             meta={"comment": completion.to_str()},
         )
 
