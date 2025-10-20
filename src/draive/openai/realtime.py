e7cee49ff77cffa0c1d8dc15e929d5ff4c7928f5
marcinwolder <marcin.wolder@miquido.com>
2025-10-20 13:55:26 +0200

    conversation.item.done event implementation

--- src/draive/openai/realtime.py
+++ src/draive/openai/realtime.py
@@ -346,6 +346,41 @@ class OpenAIRealtime(OpenAIAPI):
                         case "conversation.item.input_audio_transcription.completed":
                             await connection.conversation.item.retrieve(item_id=event.item_id)
 
+                        case "conversation.item.done":
+                            if event.item.id is None:
+                                continue  # can't use items without item id
+
+                            if event.item.type != "message":
+                                continue  # handle only messages
+
+                            # Only record completed items, otherwise request once more
+                            if event.item.status != "completed":
+                                continue  # retry getting completed event
+
+                            assert event.item.content  # nosec: B101
+                            match event.item.role:
+                                case "assistant":
+                                    try:
+                                        await memory.remember(
+                                            ModelOutput.of(
+                                                _assistant_content_to_multimodal(
+                                                    event.item.content,
+                                                    audio_format=output_audio_format,
+                                                ),
+                                                meta=current_items[event.item.id],
+                                            ),
+                                        )
+
+                                    except Exception as exc:
+                                        ctx.log_error(
+                                            "Failed to remember model context",
+                                            exception=exc,
+                                        )
+                                        raise exc
+
+                                case _:
+                                    continue  # skip other
+
                         case "conversation.item.retrieved":
                             if event.item.id is None:
                                 continue  # can't use items without item id
@@ -367,25 +402,6 @@ class OpenAIRealtime(OpenAIAPI):
                                                 _user_content_to_multimodal(
                                                     event.item.content,
                                                     audio_format=input_audio_format,
-                                                ),
-                                                meta=current_items[event.item.id],
-                                            ),
-                                        )
-
-                                    except Exception as exc:
-                                        ctx.log_error(
-                                            "Failed to remember model context",
-                                            exception=exc,
-                                        )
-                                        raise exc
-
-                                case "assistant":
-                                    try:
-                                        await memory.remember(
-                                            ModelOutput.of(
-                                                _assistant_content_to_multimodal(
-                                                    event.item.content,
-                                                    audio_format=output_audio_format,
                                                 ),
                                                 meta=current_items[event.item.id],
                                             ),
@@ -842,5 +858,21 @@ def _assistant_content_to_multimodal(
     for part in content:
         if part.text:
             parts.append(TextContent.of(part.text))
+
+        if part.audio:
+            parts.append(
+                ResourceContent.of(
+                    part.audio,
+                    mime_type=audio_format,
+                )
+            )
+
+        if part.transcript:
+            parts.append(
+                TextContent.of(
+                    part.transcript,
+                    meta={"transcript": True},
+                )
+            )
 
     return MultimodalContent.of(*parts)
